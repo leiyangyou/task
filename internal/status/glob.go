ff35936e69f73ccdcecbea65da117c00017bf4b7
Lei Yang <leiyangyou@gmail.com>
2019-07-02 08:22:10 +0800

    allow exclusion of glob patterns

--- internal/status/glob.go
+++ internal/status/glob.go
@@ -18,11 +18,38 @@ func glob(dir string, globs []string) (files []string, err error) {
 		if err != nil {
 			return nil, err
 		}
+
+		var exclude = false
+
+		if g[0] == '!' {
+			exclude = true
+			g = g[1:]
+		}
 		f, err := zglob.Glob(g)
 		if err != nil {
 			continue
 		}
-		files = append(files, f...)
+		if exclude {
+			var temp = make([]string, 0)
+			for _, x := range files {
+				var found = false
+
+				for _, y := range f {
+					if x == y {
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					temp = append(temp, x)
+				}
+			}
+			files = temp
+
+		} else {
+			files = append(files, f...)
+		}
 	}
 	sort.Strings(files)
 	return
